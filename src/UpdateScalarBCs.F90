479207ef54effb3c29fb847d9d03bbf6f35bf3ea
Chris Howland <c.j.howland@utwente.nl>
2021-03-11 14:34:25 +0100

    fix typos and modify coefficients

--- src/UpdateScalarBCs.F90
+++ src/UpdateScalarBCs.F90
@@ -10,20 +10,20 @@
 
 subroutine UpdateBCs
     use param
-    use local_arrays, only: temp
+    use local_arrays, only: temp, sal
     use mgrd_arrays
     use decomp_2d
     implicit none
     integer :: ic, jc, icr, jcr
     real, dimension(4,4) :: qv2
     real, dimension(4) :: qv1
-    real :: dxb, dxbr, delT, a, b, c1, c2, aa, bb, cc, melt
+    real :: dxb, dxbr, delT, a, b, c1, c2, aa, bb, cc, m
 
     ! Interpolate (T(1,:,:) to refined grid in y and z)
     do ic=xstart(3)-1,xend(3)
         do jc=xstart(2)-1,xend(2)
             qv2(:,:) = temp(1,jc-1:jc+2,ic-1:ic+2)
-            do icr=max(krangs(ic,1),1),min(krangs(ic+1)-1,nzmr)
+            do icr=max(krangs(ic),1),min(krangs(ic+1)-1,nzmr)
                 qv1(:) = qv2(:,1)*czrs(1,icr) + qv2(:,2)*czrs(2,icr) &
                         +qv2(:,3)*czrs(3,icr) + qv2(:,4)*czrs(4,icr)
                 do jcr=max(jrangs(jc),1),min(jrangs(jc+1)-1,nymr)
@@ -45,16 +45,16 @@ subroutine UpdateBCs
     a = 920.0/1025.0*3974.0/3.35e5/delT*pect
     b = 920.0/1025.0*pecs*25 ! 25 factor to get Sc=2500
     c1 = -5.73e-2 * 35.0 /delT
-    c2 = (8.32e-2 + 7.61e-2)/delT
+    c2 = c1 + (8.32e-2 + 7.61e-2)/delT - 1.0
     do icr=xstartr(3),xendr(3)
         do jcr=xstartr(2),xendr(2)
             aa = dxb * dxbr * a * b
             bb = dxb*a + dxbr*b*c2 - dxbr*b*tempr(jcr,icr)
             cc = c1*sal(1,jcr,icr) + c2 - tempr(jcr,icr)
-            melt = (-bb + sqrt(bb**2 - 4*aa*cc))/2/aa
-            salbp(1,jcr,icr) = (1-dxbr*b*melt)/(1+dxbr*b*melt)*sal(1,jcr,icr)
+            m = (-bb + sqrt(bb**2 - 4*aa*cc))/2/aa
+            salbp(1,jcr,icr) = (1-dxbr*b*m)/(1+dxbr*b*m)*sal(1,jcr,icr)
             saltp(1,jcr,icr) = 0.d0
-            tempr(jcr,icr) =  tempr(jcr,icr) - 2*dxb*a*melt
+            tempr(jcr,icr) =  tempr(jcr,icr) - 2*dxb*a*m
         end do
     end do
 
