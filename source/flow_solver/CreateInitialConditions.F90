e1448689c6042b4cbdbcea47efa9292b12345598
Chris Howland <c.j.howland@utwente.nl>
2021-06-16 16:12:51 +0200

    Tidy up initial conditions

--- source/flow_solver/CreateInitialConditions.F90
+++ source/flow_solver/CreateInitialConditions.F90
@@ -15,37 +15,50 @@ subroutine CreateInitialConditions
     use mpih
     implicit none
     integer :: j,k,i,kmid
-    real :: xxx,yyy,eps,varptb,amp
+    real :: xxx,yyy,zzz,eps,varptb,amp
 
     call random_seed()
-    eps=1d-2
-    do i=xstart(3),xend(3)
-        do j=xstart(2),xend(2)
-            do k=1,nxm
-                call random_number(varptb)
-                vz(k,j,i)=eps*(2.d0*varptb - 1.d0)
-                yyy=xm(k) 
-                xxx=yc(j)            
-                call random_number(varptb)
-                !  varptb=0.d0
-                !  vy(k,j,i)=-yyy*(1.d0-yyy)*(2.d0*yyy-1.d0)*sin(2.d0*pi*xxx/ylen)/pi
-                !  vy(k,j,i)=(xm(k))+0.3*(2.d0*varptb-1.d0)+ &
-                !  vy(k,j,i)=+0.3*(2.d0*varptb-1.d0)+ &
-                !     &  (2.0d0*yyy-6.0d0*yyy**2+4.0d0*yyy**3) &
-                !     &  *sin(3*xxx)*eps
 
-                !  yyy=xc(k)          
-                !  xxx=ym(j)
-                !  vx(k,j,i)=-yyy**2*(1.0d0-yyy)**2*cos(2.d0*pi*xxx/ylen)*eps
-                !  vx(k,j,i)=1.d0 ! -yyy**2*(1.0d0-yyy)**2*cos(3.1*xxx)*eps
-                !CJH Laminar vertical convection as Batchelor (54) + noise
-                vy(k,j,i) = min(ren,320.0)/12.d0*yyy*(2*yyy-1)*(yyy-1) + eps*(2.d0*varptb - 1.d0)
-                call random_number(varptb)
-                vx(k,j,i) = eps*(2.d0*varptb - 1.d0)
+    if ((xminusU.ne.0) .or. (xplusU.ne.0)) then
+        !CJH: Base velocity profile if walls in motion
+        do i=xstart(3),xend(3)
+            do j=xstart(2),xend(2)
+                do k=1,nxm
+                    xxx = xm(k)
+                    vy(k,j,i) = xminusU + (xplusU - xminusU)*xxx/alx3
+                end do
             end do
-            vx(1,j,i) = 0.d0   !CJH Enforce no-slip BC at x=0
         end do
-    end do
+    end if
+
+    if (gAxis.eq.1) then
+        !CJH: RBC initial condition as used in AFiD 1.0
+        eps = 0.01
+        do i=xstart(3),xend(3)
+            do j=xstart(2),xend(2)
+                do k=1,nxm
+                    xxx = xc(k)
+                    yyy = ym(j)
+                    vx(k,j,i) = vx(k,j,i) - eps*xxx**2*(1.0 - xxx)**2*cos(2.0*pi*yyy/ylen)
+
+                    xxx = xm(k)
+                    yyy = yc(j)
+                    vy(k,j,i) = vy(k,j,i) + 2.0*eps*xxx*(1.0 - xxx)*(1.0 - 2.0*xxx)*sin(2.0*pi*yyy/ylen)/pi
+                end do
+            end do
+        end do
+    elseif (gAxis.eq.2) then
+        !CJH Laminar vertical convection as in Batchelor (1954)
+        eps = 1e-3
+        do i=xstart(3),xend(3)
+            do j=xstart(2),xend(2)
+                do k=1,nxm
+                    xxx = xm(k)
+                    vy(k,j,i) = min(ren,320.0)/12.0*xxx*(2*xxx-1)*(xxx-1)
+                end do
+            end do
+        end do
+    end if
 
     if (melt .or. phasefield) then
         do i=xstart(3),xend(3)
@@ -59,46 +72,73 @@ subroutine CreateInitialConditions
         end do
     end if
 
-    !assign linear temperature profile in the nodes k=1 to k=nxm
-    eps=0.1 !0.d0!5d-2
-    kmid = nxm/2
+    ! Assign linear temperature profile in the nodes k=1 to k=nxm
+    do i=xstart(3),xend(3)
+        do j=xstart(2),xend(2)
+            do k=1,nxm
+                xxx = xm(k)
+                temp(k,j,i) = tempbp(1,j,i) + (temptp(1,j,i) - tempbp(1,j,i))*xm(k)/alx3
+            end do
+        end do
+    end do
+
+    ! Add noise in the temperature profile
+    eps = 1e-3
     do i=xstart(3),xend(3)
         do j=xstart(2),xend(2)
             do k=1,nxm
                 call random_number(varptb)
-                temp(k,j,i)=tempbp(1,j,i)+(temptp(1,j,i)-tempbp(1,j,i))*xm(k)/xc(nx)
-                ! if (abs(xm(k)-0.5) + eps > 0.5) then
-                !   amp = 0.5 - abs(xm(k)-0.5) ! CJH Prevent values of |T| exceeding 0.5
-                !   temp(k,j,i) = temp(k,j,i) + amp*(2.d0*varptb - 1.d0)
-                ! else
-                !   temp(k,j,i) = temp(k,j,i) + eps*(2.d0*varptb - 1.d0)
-                ! end if
+                if (abs(xm(k)-0.5) + eps > 0.5) then
+                  amp = 0.5 - abs(xm(k)-0.5) ! CJH Prevent values of |T| exceeding 0.5
+                  temp(k,j,i) = temp(k,j,i) + amp*(2.d0*varptb - 1.d0)
+                else
+                  temp(k,j,i) = temp(k,j,i) + eps*(2.d0*varptb - 1.d0)
+                end if
             end do
-            !CJH Favier et al (2019) Appendix A3 Validation Case
-            if (nym.gt.1) then
-                do k=1,kmid ! If domain 3D, add in z perturbation too
-                    temp(k,j,i) = temp(k,j,i) &
-                        + eps*sin(4*pi*ym(j))*cos(4*pi*zm(i))*sin(2*pi*xm(k))**2
-                end do
-            else
-                do k=1,kmid
-                    temp(k,j,i) = temp(k,j,i) &
-                        + eps*sin(4*pi*ym(j))*sin(2*pi*xm(k))**2
-                end do
-            end if
         end do
     end do
-    if (salinity) then
+
+    if (phasefield) then
+
+        eps = 0.1
+        kmid = nxm/2
         do i=xstart(3),xend(3)
             do j=xstart(2),xend(2)
-                do k=1,kmid
-                    temp(k,j,i) = 1.0
-                end do
-                do k=kmid+1,nxm
-                    temp(k,j,i) = 0.0
-                end do
+            !CJH Favier et al (2019) Appendix A3 Validation Case
+                if (nzm.gt.1) then
+                    do k=1,kmid ! If domain 3D, add in z perturbation too
+                        xxx = xm(k)
+                        yyy = ym(j)
+                        zzz = zm(i)
+
+                        temp(k,j,i) = temp(k,j,i) &
+                            + eps*sin(4.0*pi*yyy)*cos(4.0*pi*zzz)*sin(2.0*pi*xxx)**2
+                    end do
+                else
+                    do k=1,kmid
+                        xxx = xm(k)
+                        yyy = ym(j)
+
+                        temp(k,j,i) = temp(k,j,i) &
+                            + eps*sin(4.0*pi*yyy)*sin(2.0*pi*xxx)**2
+                    end do
+                end if
             end do
         end do
+
+        if (salinity) then
+            do i=xstart(3),xend(3)
+                do j=xstart(2),xend(2)
+                    do k=1,kmid
+                        temp(k,j,i) = 1.0
+                    end do
+                    do k=kmid+1,nxm
+                        temp(k,j,i) = 0.0
+                    end do
+                end do
+            end do
+        end if
+
     end if
 
     if (melt) then
