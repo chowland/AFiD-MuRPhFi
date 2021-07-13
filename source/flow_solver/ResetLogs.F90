a7137ac49430bafff26559e876b4405989211bb5
Chris Howland <c.j.howland@utwente.nl>
2021-07-13 15:58:46 +0100

    Remove unused reset commands

--- source/flow_solver/ResetLogs.F90
+++ source/flow_solver/ResetLogs.F90
@@ -15,14 +15,14 @@
       if(resetlogstime) then    
 
 !EP    nusse.out  in GlobalQuantities.F
-      open(95,file='outputdir/nu_vol.out',status='unknown',access='sequential', &
-        position='append')
-      close(95,status='delete')
+      ! open(95,file='outputdir/nu_vol.out',status='unknown',access='sequential', &
+      !   position='append')
+      ! close(95,status='delete')
 
 !EP    nusse2.out  in CalculatePlateNu.F
-       open(97,file="outputdir/nu_plate.out",status='unknown',access='sequential', &
-        position='append')
-      close(97,status='delete')
+      !  open(97,file="outputdir/nu_plate.out",status='unknown',access='sequential', &
+      !   position='append')
+      ! close(97,status='delete')
 
 !EP   nusse3.out in CalcDissipationNu.F
       ! if (disscal) then
@@ -32,9 +32,9 @@
       ! end if
 
 !EP   rms_vel.out in GlobalQuantities.F
-       open(94,file='outputdir/rms_vel.out',status='unknown',position='append', &
-        access='sequential')
-      close(94,status='delete')
+      !  open(94,file='outputdir/rms_vel.out',status='unknown',position='append', &
+      !   access='sequential')
+      ! close(94,status='delete')
 
 !CS   cfl.out in main.F90                                                    
        open(96,file='outputdir/cfl.out',status='unknown',position='append', &
@@ -42,9 +42,9 @@
       close(96,status='delete')
 
 !CS    cf.out  in CalculatePlateCf.F
-       open(98,file="outputdir/cf_plate.out",status='unknown',access='sequential', &
-        position='append')
-      close(98,status='delete')
+      !  open(98,file="outputdir/cf_plate.out",status='unknown',access='sequential', &
+      !   position='append')
+      ! close(98,status='delete')
 
       endif
 
