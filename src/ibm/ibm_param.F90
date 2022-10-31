924f457e241896a402014d1494a1f33adbf424db
chowland <c.j.howland@utwente.nl>
2022-10-31 09:16:41 +0100

    tidy up unused variables

--- src/ibm/ibm_param.F90
+++ src/ibm/ibm_param.F90
@@ -3,21 +3,18 @@ module ibm_param
       integer :: npunx,npuny,npunz,npunte,mpun,mpunr,npuntr
       integer :: n, solidtype
       real :: aldto
-      parameter (mpun=200000)
+      parameter (mpun=50000)
       parameter (mpunr=300000)
       real,allocatable,dimension(:,:) :: plth1, plth2
       logical,allocatable,dimension(:,:,:) :: solidr
       integer,allocatable,dimension(:,:,:) :: ibmaskx,ibmasky,ibmaskz
       integer,allocatable,dimension(:,:,:) :: ibmaskt, ibmaskr
-      ! integer,dimension(3,mpun,3) :: indgeo, indgeoe
-      ! integer,dimension(mpun,3) :: indgeot, indgeoet
-      integer,dimension(mpun,3) :: indgeor, indgeoer
-      ! integer,dimension(mpun) :: indgeorup, indgeorlo
       real,dimension(mpun,3) :: distb
       real,dimension(mpun) :: distx, disty, distz
-      real,dimension(mpun) :: distbt,distbr!up,distbrlo
-      real,dimension(mpun) :: temb,salfix
-      real,dimension(mpun) :: q1bo,q2bo,q3bo,densb,salb
+      real,dimension(mpun) :: distbt
+      real,dimension(mpunr) :: distbr
+      ! real,dimension(mpun) :: temb,salfix
+      ! real,dimension(mpun) :: q1bo,q2bo,q3bo,densb,salb
       
 end module ibm_param
 
