a67a1343dbf16bb97c4674461a81828334a20f34
chowland <c.j.howland@utwente.nl>
2023-04-18 11:51:03 +0200

    improve formatting

--- src/flow_solver/ExplicitTermsTemp.F90
+++ src/flow_solver/ExplicitTermsTemp.F90
@@ -8,128 +8,79 @@
 !                                                         !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-      subroutine ExplicitTermsTemp
-      use param
-      use local_arrays, only: vy,vx,temp,vz,hro
-      use decomp_2d, only: xstart,xend
-      implicit none
-      integer :: jc,kc,ic
-      integer :: km,kp,jm,jp,im,ip
-      real    :: htx,hty,htz,udy,udz
-      real    :: udzq,udyq
-      real    :: dyyt,dzzt
+subroutine ExplicitTermsTemp
+    use param
+    use local_arrays, only: vy,vx,temp,vz,hro
+    use decomp_2d, only: xstart,xend
+    implicit none
+    integer :: jc,kc,ic
+    integer :: km,kp,jm,jp,im,ip
+    real    :: htx,hty,htz,udy,udz
+    real    :: udzq,udyq
+    real    :: dyyt,dzzt
+    
+    udz=dz*0.5d0
+    udy=dy*0.5d0
+    udzq=dzq/pect
+    udyq=dyq/pect
+    
+    !$OMP  PARALLEL DO &
+    !$OMP   DEFAULT(none) &
+    !$OMP   SHARED(xstart,xend,vz,vy,vx,nxm) &
+    !$OMP   SHARED(kmv,kpv,am3sk,ac3sk,ap3sk,udz) &
+    !$OMP   SHARED(udy,udzq,udyq,udx3c,temp,hro) &
+    !$OMP   PRIVATE(ic,jc,kc,im,ip,km,kp,jm,jp) &
+    !$OMP   PRIVATE(htx,hty,htz,dyyt,dzzt)
+    do ic=xstart(3),xend(3)
+        im=ic-1
+        ip=ic+1
+        do jc=xstart(2),xend(2)
+            jm=jc-1
+            jp=jc+1
+            do kc=1,nxm
 
-      udz=dz*0.5d0
-      udy=dy*0.5d0
-      udzq=dzq/pect
-      udyq=dyq/pect
+                ! ! x-advection d/dx(vx * T)
+                if (kc==1) then
+                    htx = ( &
+                          vx(kc+1,jc,ic)*(temp(kc+1,jc,ic) + temp(kc,jc,ic)) &
+                        - vx(kc  ,jc,ic)*2.0*tempbp(1,jc,ic) &
+                    )*0.5*udx3m(kc)
+                elseif (kc==nxm) then
+                    htx = ( &
+                          vx(kc+1,jc,ic)*2.0*temptp(1,jc,ic) &
+                        - vx(kc  ,jc,ic)*(temp(kc,jc,ic) + temp(kc-1,jc,ic)) &
+                    )*0.5*udx3m(kc)
+                else
+                    htx = ( &
+                          vx(kc+1,jc,ic)*(temp(kc+1,jc,ic) + temp(kc  ,jc,ic)) &
+                        - vx(kc  ,jc,ic)*(temp(kc  ,jc,ic) + temp(kc-1,jc,ic)) &
+                    )*0.5*udx3m(kc)
+                end if
 
-!$OMP  PARALLEL DO &
-!$OMP   DEFAULT(none) &
-!$OMP   SHARED(xstart,xend,vz,vy,vx,nxm) &
-!$OMP   SHARED(kmv,kpv,am3sk,ac3sk,ap3sk,udz) &
-!$OMP   SHARED(udy,udzq,udyq,udx3c,temp,hro) &
-!$OMP   PRIVATE(ic,jc,kc,im,ip,km,kp,jm,jp) &
-!$OMP   PRIVATE(htx,hty,htz,dyyt,dzzt)
-      do ic=xstart(3),xend(3)
-       im=ic-1
-       ip=ic+1
-       do jc=xstart(2),xend(2)
-        jm=jc-1
-        jp=jc+1
+                ! ! z-advection d/dx(vz * T)
+                htz = ( &
+                      vz(kc,jc,ip)*(temp(kc,jc,ip)+temp(kc,jc,ic)) &
+                    - vz(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jc,im)) &
+                )*udz
+                
+                ! ! y-advection d/dx(vy * T)
+                hty=( &
+                      vy(kc,jp,ic)*(temp(kc,jp,ic)+temp(kc,jc,ic)) &
+                    - vy(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jm,ic)) &
+                )*udy
 
-        kc = 1
-        kp = 2
-        htx=(vx(kp,jc,ic)*(temp(kp,jc,ic) + temp(kc,jc,ic)) - &
-             vx(kc,jc,ic)*2.d0*tempbp(1,jc,ic))*udx3m(kc)*0.5d0
-        htz=(vz(kc,jc,ip)*(temp(kc,jc,ip)+temp(kc,jc,ic))- &
-             vz(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jc,im)) &
-            )*udz
-        hty=(vy(kc,jp,ic)*(temp(kc,jp,ic)+temp(kc,jc,ic))- &
-             vy(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jm,ic)) &
-            )*udy
-        dzzt=(temp(kc,jc,ip) - 2.0*temp(kc,jc,ic) + temp(kc,jc,im))*udzq
-        dyyt=(temp(kc,jp,ic) - 2.0*temp(kc,jc,ic) + temp(kc,jm,ic))*udyq
-        hro(kc,jc,ic) = -(htx+hty+htz)+dyyt+dzzt
-     !    hro(kc,jc,ic) = dyyt+dzzt
+                !   zz second derivatives of temp
+                dzzt=(temp(kc,jc,ip) - 2.0*temp(kc,jc,ic) + temp(kc,jc,im))*udzq
+                !   yy second derivatives of temp
+                dyyt=(temp(kc,jp,ic) - 2.0*temp(kc,jc,ic) + temp(kc,jm,ic))*udyq
 
-        do kc=2,nxm-1
-         km=kc-1
-         kp=kc+1
-         !
-         !    rho vx term
-         !
-         !
-         !                 d  rho q_x 
-         !                -----------
-         !                 d   x      
-         !
-               htx=(vx(kp,jc,ic)*(temp(kp,jc,ic)+temp(kc,jc,ic))- &
-                    vx(kc,jc,ic)*(temp(kc,jc,ic)+temp(km,jc,ic)) &
-                   )*udx3m(kc)*0.5d0
-!
-!
-!    rho vz term
-!
-!
-!                d  rho q_z
-!             -----------
-!                d   z      
-!
-      htz=(vz(kc,jc,ip)*(temp(kc,jc,ip)+temp(kc,jc,ic))- &
-           vz(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jc,im)) &
-          )*udz
-!
-!
-!    rho vy term
-!
-!
-!                d  rho q_y 
-!             -----------
-!                d   y      
-!
-      hty=(vy(kc,jp,ic)*(temp(kc,jp,ic)+temp(kc,jc,ic))- &
-           vy(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jm,ic)) &
-          )*udy
-!
-!
-!   zz second derivatives of temp
-!
-            dzzt=(temp(kc,jc,ip) &
-             -2.0*temp(kc,jc,ic) &
-                 +temp(kc,jc,im))*udzq
-      
-!
-!   yy second derivatives of temp
-!
-            dyyt=(temp(kc,jp,ic) &
-             -2.0*temp(kc,jc,ic) &
-                 +temp(kc,jm,ic))*udyq
-!
-            hro(kc,jc,ic) = -(htx+hty+htz)+dyyt+dzzt
-          !   hro(kc,jc,ic) = dyyt+dzzt
-        enddo
-          
-        kc = nxm
-        kp = nx
-        km = nxm - 1
-        htx=(vx(kp,jc,ic)*2.d0*temptp(1,jc,ic) - &
-             vx(kc,jc,ic)*(temp(kc,jc,ic)+temp(km,jc,ic)) &
-            )*udx3m(kc)*0.5d0
-        htz=(vz(kc,jc,ip)*(temp(kc,jc,ip)+temp(kc,jc,ic))- &
-             vz(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jc,im)) &
-            )*udz
-        hty=(vy(kc,jp,ic)*(temp(kc,jp,ic)+temp(kc,jc,ic))- &
-             vy(kc,jc,ic)*(temp(kc,jc,ic)+temp(kc,jm,ic)) &
-            )*udy
-        dzzt=(temp(kc,jc,ip) - 2.0*temp(kc,jc,ic) + temp(kc,jc,im))*udzq
-        dyyt=(temp(kc,jp,ic) - 2.0*temp(kc,jc,ic) + temp(kc,jm,ic))*udyq
-        hro(kc,jc,ic) = -(htx+hty+htz)+dyyt+dzzt
-     !    hro(kc,jc,ic) = dyyt+dzzt
-
-        enddo
-      enddo
-!$OMP  END PARALLEL DO
-
-      return
-      end
+                hro(kc,jc,ic) = -(htx+hty+htz)+dyyt+dzzt
+                !   hro(kc,jc,ic) = dyyt+dzzt
+            end do
+            
+        end do
+    end do
+    !$OMP  END PARALLEL DO
+    
+    return
+end
