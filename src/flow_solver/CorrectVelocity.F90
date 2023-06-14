335d395c464e73573e073261ba69bdbd5e70db07
chowland <c.j.howland@utwente.nl>
2023-06-14 10:17:13 +0200

    clean up old divergence removal

--- src/flow_solver/CorrectVelocity.F90
+++ src/flow_solver/CorrectVelocity.F90
@@ -22,34 +22,6 @@ subroutine CorrectVelocity
     real, dimension(nxmr):: GshapeR
     real :: vybulk, vzbulk, Tbulk, Sbulk, idx, vz_target, lam, lfac
 
-!     udy = al*dt*dy
-!     udz = al*dt*dz
-
-! !$OMP  PARALLEL DO &
-! !$OMP   DEFAULT(none) &
-! !$OMP   SHARED(vz,vy,vx,dphhalo,udz,udy,udx3c) &
-! !$OMP   SHARED(xstart,xend,nxm,kmv,dt,al) &
-! !$OMP   PRIVATE(ic,jc,kc) &
-! !$OMP   PRIVATE(im,jm,km,usukm,locdph)
-!     do ic=xstart(3),xend(3)
-!         im=ic-1
-!         do jc=xstart(2),xend(2)
-!             jm=jc-1
-!             do kc=1,nxm
-!                 km=kmv(kc)
-!                 usukm = al*dt*udx3c(kc)
-!                 locdph=dphhalo(kc,jc,ic)
-!                 vx(kc,jc,ic)=vx(kc,jc,ic)- &
-!                     (locdph-dphhalo(km,jc,ic))*usukm
-!                 vy(kc,jc,ic)=vy(kc,jc,ic)- &
-!                     (locdph-dphhalo(kc,jm,ic))*udy
-!                 vz(kc,jc,ic)=vz(kc,jc,ic)- &
-!                     (locdph-dphhalo(kc,jc,im))*udz
-!             enddo
-!        enddo
-!     enddo
-! ! $OMP END PARALLEL DO
-
     call RemoveDivergence
 
     !CJH Prescribe mean volume flux
