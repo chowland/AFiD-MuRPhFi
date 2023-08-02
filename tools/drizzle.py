792f2462af31882baee260ab2ae36f22ae0cfe23
chowland <c.j.howland@utwente.nl>
2023-08-02 23:59:17 +0300

    add option for unsaturated top boundary

--- tools/drizzle.py
+++ tools/drizzle.py
@@ -9,9 +9,14 @@ inputs = afid.InputParams(fld)
 nxm = inputs.nxm
 print(nxm)
 
+saturated_top = False
+
 alpha = 3.0
 beta = 2.0
-gamma = beta/(1 - np.exp(-alpha))
+if saturated_top:
+    gamma = beta/(1 - np.exp(-alpha))
+else:
+    gamma = beta
 print(alpha, beta, gamma)
 
 def W(z):
@@ -23,7 +28,10 @@ z = 0.5*(zc[1:] + zc[:-1])
 
 # Set moist static energy
 P = gamma
-Q = beta - 1 - gamma*(1 - np.exp(-alpha))
+if saturated_top:
+    Q = beta - 1 - gamma*(1 - np.exp(-alpha))
+else:
+    Q = beta - 1 - gamma
 m = P + Q*z
 
 # Define temperature profile
@@ -35,6 +43,15 @@ b = T + beta*z
 
 # Compute humidity profile
 q = (m - b)/gamma
+
+if not saturated_top:
+    # Calculate height above which humidity not at saturation
+    D = alpha*(beta + 1)*np.exp(1-alpha)/(1 + alpha*beta*np.exp(1-alpha))
+    B = beta*D - 1
+    zc = 1 + 1/alpha/(B-beta)
+    # Prescribe linear profiles above this level
+    b[z > zc] = beta - 1 + B*(z[z > zc] - 1)
+    q[z > zc] = D*(1 - z[z > zc])
 
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots(layout='constrained')
