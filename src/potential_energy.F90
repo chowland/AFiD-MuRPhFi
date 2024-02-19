b538d2e1fe374c6ec9268c0e6fd9ddbeeb2c2a2e
chowland <c.j.howland@utwente.nl>
2024-02-19 14:38:40 +0100

    update accuracy of APE calculation

--- src/potential_energy.F90
+++ src/potential_energy.F90
@@ -5,12 +5,12 @@ module afid_APE
     use afid_salinity
     implicit none
 
-    integer :: nb=nxmr  !< Number of bins for the sorted density
-    real :: zref(nb)    !< Reference vertical coordinate profile Z(rho)
-    real :: edges(nb+1) !< Locations of bin edges for density sorting
-    real :: bmids(nb)   !< Midpoints of the bins for density
-    real :: bmin = -1.0_dp      !< Minimum density
-    real :: bmax = 1.0_dp       !< Maximum density
+    integer :: nb       !< Number of bins for the sorted density
+    real, allocatable :: zref(:)    !< Reference vertical coordinate profile Z(rho)
+    real, allocatable :: edges(:)   !< Locations of bin edges for density sorting
+    real, allocatable :: bmids(:)   !< Midpoints of the bins for density
+    real :: bmin = -1.d0      !< Minimum density
+    real :: bmax = 1.d0       !< Maximum density
     real :: bw          !< Bin width
     
 contains
@@ -18,6 +18,11 @@ contains
 !> Define the bin edges
 subroutine init_bin_edges
     integer :: n
+
+    nb = nxmr
+    allocate(zref(nb+1))
+    allocate(edges(nb+1))
+    allocate(bmids(nb))
 
     ! Set the bin edges, spanning the range [-1,1]
     bw = (bmax - bmin)/nb
@@ -33,18 +38,23 @@ subroutine sort_salinity
     real :: bpdf(nb)    !< PDF for density
     real :: dxc(nxmr)   !< Vertical grid spacing of refined grid
     integer :: i, j, k, n
+    real :: zscale
+    logical :: exists
+    integer :: io, stat
 
     ! Compute grid spacing for volume weighting
     do k=1,nxmr
         dxc(k) = xcr(k+1) - xcr(k)
     end do
 
+    bpdf(:) = 0.0
+
     ! Compute the volume-weighted histogram
     do i=xstartr(3),xendr(3)
         do j=xstartr(2),xendr(2)
             do k=1,nxmr
                 do n=1,nb
-                    if (sal(k,j,i) < edges(n+1) .and. sal(k,j,i) > edges(n-1)) then
+                    if (sal(k,j,i) < edges(n+1) .and. sal(k,j,i) > edges(n)) then
                         bpdf(n) = bpdf(n) + dxc(k)
                     end if
                 end do
@@ -55,13 +65,32 @@ subroutine sort_salinity
     call MpiAllSumReal1D(bpdf, nb)
 
     ! Normalize the pdf such that its integral is one
-    bpdf = bpdf/sum(bpdf)*nb
+    bpdf = bpdf/sum(bpdf)*nb/(bmax - bmin)
 
     ! Construct a reference vertical coordinate
-    zref(1) = bpdf(1)*bw
-    do n=2,nb
-        zref(n) = zref(n-1) + bpdf(n)*bw
+    zscale = bw*alx3
+    zref(1) = alx3
+    do n=1,nb
+        zref(n+1) = zref(n) - bpdf(n)*zscale
     end do
+
+    if (ismaster) then
+        ! call HdfSerialWriteReal1D()
+        inquire(file="Zr.txt", exist=exists)
+        if (.not. exists) then
+            open(file="Zr.txt", newunit=io, iostat=stat, &
+            status="new", action="write")
+            write(io, *) zref
+            close(io)
+        end if
+        inquire(file="pdf.txt", exist=exists)
+        if (.not. exists) then
+            open(file="pdf.txt", newunit=io, iostat=stat, &
+            status="new", action="write")
+            write(io, *) bpdf
+            close(io)
+        end if
+    end if
 
 end subroutine sort_salinity
 
@@ -72,18 +101,27 @@ function Zstar(vsal) result(zz)
     real :: zz
     integer :: n
 
-    if (vsal < bmids(1)) then
-        zz = (vsal - bmin)*0.5/bw*zref(1)
-    elseif (vsal > bmids(nb)) then
-        zz = zref(nb) + (vsal - bmids(nb))*0.5/bw*(1.0 - zref(nb))
-    else
-        do n=1,nb-1
-            if (vsal < bmids(n+1) .and. vsal > bmids(n)) then
-                zz = zref(n) + (vsal - bmids(n))/bw*(zref(nb+1) - zref(nb))
-            end if
-        end do
-    end if
+    do n=1,nb
+        if (vsal < edges(n+1) .and. vsal > edges(n)) then
+            zz = zref(n) + (vsal - edges(n))*(zref(n+1) - zref(n))/bw
+        end if
+    end do
+
 end function Zstar
+
+!> Interpolate the profile C(xx) stored implicitly by `zref`
+!! to the height given by xx
+function Cstar(xx) result(C)
+    real, intent(in) :: xx !< Vertical position
+    real :: C
+    integer :: n
+
+    do n=1,nb
+        if (xx > zref(n+1) .and. xx < zref(n)) then
+            C = edges(n) + (xx - zref(n))*bw/(zref(n+1) - zref(n))
+        end if
+    end do
+end function Cstar
 
 
 !> Compute the potential energy of the salinity field, along with the
@@ -92,11 +130,17 @@ subroutine compute_potential_energy
     real :: rho         !< dimensionless density perturbation
     real :: E_P = 0.0       !< potential energy <rho*g*z>
     real :: E_B = 0.0       !< background potential energy <rho*g*Z(rho)>
+    real :: E_B2       !< background potential energy <rho_b*g*z>
     real :: E_A         !< available potential energy E_A = E_P - E_B
+    real :: E_A2         !< available potential energy E_A = E_P - E_B2
     real :: dxc         !< vertical grid spacing (for integration in x)
     real :: inv         !< 1/Lx/ny/nz for normalisation of integration
+    real :: Cs(nxmr), Zs(nxmr)
     logical :: exists
     integer :: i, j, k, io, stat
+    character(5) :: nstat
+
+    call sort_salinity
 
     do i=xstartr(3),xendr(3)
         do j=xstartr(2),xendr(2)
@@ -109,13 +153,51 @@ subroutine compute_potential_energy
         end do
     end do
 
+    Cs(:) = 0.0
+
+    E_B2 = 0.0
+    do k=1,nxmr
+        Cs(k) = Cstar(xmr(k))
+        Zs(k) = Zstar(-tanh(xmr(k) - 0.5*alx3))
+        E_B2 = E_B2 + xmr(k)*(xcr(k+1) - xcr(k))*Cs(k)
+    end do
+    E_B2 = E_B2/alx3
+    if (ismaster) then
+        write(nstat,"(i5.5)") nint(time/tout)
+        inquire(file="Cs.h5", exist=exists)
+        if (exists) then
+            call HdfSerialWriteReal1D(nstat, "Cs.h5", Cs, nxmr)
+        else
+            call HdfCreateBlankFile("Cs.h5")
+            call HdfSerialWriteReal1D(nstat, "Cs.h5", Cs, nxmr)
+        end if
+
+        inquire(file="Cs.txt", exist=exists)
+        if (.not. exists) then
+            open(file="Cs.txt", newunit=io, iostat=stat, &
+            status="new", action="write")
+            write(io, *) Cs(:)
+            close(io)
+        end if
+
+        inquire(file="Zs.txt", exist=exists)
+        if (.not. exists) then
+            open(file="Zs.txt", newunit=io, iostat=stat, &
+            status="new", action="write")
+            write(io, *) Zs
+            close(io)
+        end if
+    end if
+
     call MpiSumRealScalar(E_P)
     call MpiSumRealScalar(E_B)
 
-    inv = 1.0_dp/alx3/nymr/nzmr
+    inv = 1.d0/alx3/nymr/nzmr
     E_P = E_P*inv
     E_B = E_B*inv
+    ! E_B2 = E_B2*inv
     E_A = E_P - E_B
+    E_A2 = E_P - E_B2
 
     if (ismaster) then
         inquire(file="PE.txt", exist=exists)
@@ -126,7 +208,7 @@ subroutine compute_potential_energy
             open(file="PE.txt", newunit=io, iostat=stat, &
             status="new", action="write")
         end if
-        write(io, *) E_P, E_B, E_A
+        write(io, *) E_P, E_B, E_B2, E_A, E_A2
         close(io)
     end if
 
