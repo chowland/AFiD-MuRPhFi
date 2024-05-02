4fca439bffe379650b9e5756f64f5a03185e0aea
chowland <c.j.howland@utwente.nl>
2024-05-02 21:59:42 +0200

    update BC halo for sidewall case

--- src/flow_solver/SetTempBCs.F90
+++ src/flow_solver/SetTempBCs.F90
@@ -13,7 +13,7 @@ subroutine SetTempBCs
     use decomp_2d
     use afid_moisture, only: beta_q
     implicit none
-    integer :: ic,jc
+    integer :: ic,jc,n
     
     if (rayt>=0) then ! unstable T gradient
         if (inslwN==0) then !Single heated wall case
@@ -69,6 +69,34 @@ subroutine SetTempBCs
     
     call update_halo(temptp,lvlhalo)
     call update_halo(tempbp,lvlhalo)
+
+    !! CJH: May want updating to think about how to properly deal with corners
+    if (sidewall) then
+        if (xstart(2)==1) then
+            do n=1,lvlhalo
+                tempbp(1,1-n,:) = tempbp(1,n,:)
+                temptp(1,1-n,:) = temptp(1,n,:)
+            end do
+        end if
+        if (xend(2)==nym) then
+            do n=1,lvlhalo
+                tempbp(1,nym+n,:) = tempbp(1,ny-n,:)
+                temptp(1,nym+n,:) = temptp(1,ny-n,:)
+            end do
+        end if
+        if (xstart(3)==1) then
+            do n=1,lvlhalo
+                tempbp(1,:,1-n) = tempbp(1,:,n)
+                temptp(1,:,1-n) = temptp(1,:,n)
+            end do
+        end if
+        if (xend(3)==nzm) then
+            do n=1,lvlhalo
+                tempbp(1,:,nzm+n) = tempbp(1,:,nz-n)
+                temptp(1,:,nzm+n) = temptp(1,:,nz-n)
+            end do
+        end if
+    end if
     
     return
 end subroutine SetTempBCs
